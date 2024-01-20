e09a5d000e80c70495f7390d1b86bd635bf5b74c
AnaJulia <anaju.silvaalves@gmail.com>
2024-01-19 23:57:36 -0300

    custom game-over

--- script.js
+++ script.js
@@ -12,28 +12,27 @@ const jump = () => {
     } , 1000);
 }
 
+
+
 const loop = setInterval(() => {
-
-    console.log(loop);  
-
+    
     const housePosition = house.offsetLeft;
     const catbusPosition = +window.getComputedStyle(catbus).bottom.replace('px' , '');
-
-
+    
+    
     if (housePosition <= 200  && housePosition > 0 && catbusPosition < 130){
-
+        
         house.style.animation = 'none';
         house.style.left = `${housePosition}px`;
-
+        
         catbus.style.animation = 'none';
         catbus.style.bottom = `${catbusPosition}px`;
-
-        catbus.
         
-        src = './assets/game-over.png'
-        catbus.style.width = '130px';
-        catbus.style.marginleft = '0px';
 
+        catbus.src = './assets/catbus-gameover.png';
+        catbus.style.width = '200px';
+        catbus.style.marginleft = '0px';
+        
         clearInterval(loop);
         
         crash();
@@ -43,7 +42,13 @@ const loop = setInterval(() => {
 
 function crash (){
     gameover.classList.add('over');
+
     gameboard.classList.add('over');
+
+    house.classList.add('inverter');
+    catbus.classList.add('inverter');
+
+
 }
 
 
